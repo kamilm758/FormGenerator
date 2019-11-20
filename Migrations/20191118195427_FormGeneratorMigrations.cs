729885baf087bf0e225803eef93b73e6c2778f9a
kamilm758 <48297624+kamilm758@users.noreply.github.com>
2019-11-20 22:49:44 +0100

    Initialize

---
+++ Migrations/20191118195427_FormGeneratorMigrations.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FormGenerator.Migrations
+{
+    public partial class FormGeneratorMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Field",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Field", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormField",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdForm = table.Column<int>(nullable: false),
+                    IdField = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormField", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Froms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Froms", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Field");
+
+            migrationBuilder.DropTable(
+                name: "FormField");
+
+            migrationBuilder.DropTable(
+                name: "Froms");
+        }
+    }
+}
---
+++ Models/Modele pomocnicze/IdFieldBool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormGenerator.Models.Modele_pomocnicze
+{
+    public class IdFieldBool
+    {
+        public int IdField { get; set; }
+        public string NameField { get; set; }
+        public bool? ContainsField { get; set; }
+    }
+}
